73545556c3d69f46cefdd8a784d0643eb4118e8d
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-11-09 10:59:16 +0500

    Signup Screen Created

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './src/screens/Welcome';
 import Login from './src/screens/Login';
+import Signup from './src/screens/Signup';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Welcome/> */}
-      <Login/>
+      {/* <Login/> */}
+     <Signup/>
       </View>
   );
 }
